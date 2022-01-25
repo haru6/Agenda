a79c78deee563765bd305e5b7837723435915696
robson.presotti <rpresotti6@gmail.com>
2022-01-24 21:49:27 -0300

    Criação do formulario visual

---
+++ app/src/main/java/com/example/agenda/FormularioActivy.kt
@@ -0,0 +1,11 @@
+package com.example.agenda
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FormularioActivy : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.formulario_activy)
+    }
+}
