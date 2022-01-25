88db8b617dee5df28c80c2953e133112eabdf0a2
robson.presotti <rpresotti6@gmail.com>
2022-01-25 14:08:28 -0300

    Criação da barra de menu

--- app/src/main/java/com/example/agenda/FormularioActivy.kt
+++ app/src/main/java/com/example/agenda/FormularioActivy.kt
@@ -2,9 +2,13 @@ package com.example.agenda
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import java.util.zip.Inflater
 
 class FormularioActivy : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +23,24 @@ class FormularioActivy : AppCompatActivity() {
             Toast.makeText(
                 this@FormularioActivy,
                 "Botão Clicado",
-                Toast.LENGTH_SHORT
-            ).show() })
+                Toast.LENGTH_SHORT,
+            ).show()
+        finish()
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_formulario, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.menu_formulario_ok ->
+                Toast.makeText(this@FormularioActivy, "Aluno Salvo", Toast.LENGTH_SHORT).show()
+        }
+        finish()
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/example/agenda/MainActivity.kt
+++ app/src/main/java/com/example/agenda/MainActivity.kt
@@ -1,15 +1,17 @@
 package com.example.agenda
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.*
-import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         criarLista()
+        adicionarAluno()
     }
 
     private fun criarLista(){
@@ -17,6 +19,14 @@ class MainActivity : AppCompatActivity() {
         val tv_alunos: ListView = findViewById(R.id.lista_alunos)
         val adapter: ArrayAdapter<String> = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos)
         tv_alunos.adapter = adapter
+    }
+
+    private fun adicionarAluno(){
+        val btn_aluno: Button = findViewById(R.id.btn_add)
+        btn_aluno.setOnClickListener(View.OnClickListener {
+            val intentVaiProFormulario: Intent = Intent(this@MainActivity, FormularioActivy::class.java)
+            startActivity(intentVaiProFormulario)
+        })
     }
 
 }
