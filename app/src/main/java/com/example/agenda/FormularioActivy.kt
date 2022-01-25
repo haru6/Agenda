83d5adfbdb13e9ffbbbe1afd2ec4e9a542d632bc
robson.presotti <rpresotti6@gmail.com>
2022-01-25 09:30:31 -0300

    Criando a função de click do botao

--- app/src/main/java/com/example/agenda/FormularioActivy.kt
+++ app/src/main/java/com/example/agenda/FormularioActivy.kt
@@ -2,10 +2,24 @@ package com.example.agenda
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class FormularioActivy : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.formulario_activy)
+        botaoSalvar()
+    }
+
+    private fun botaoSalvar(){
+        val btn_salvar: Button = findViewById(R.id.formulario_btn_salvar)
+        btn_salvar.setOnClickListener(View.OnClickListener {
+            Toast.makeText(
+                this@FormularioActivy,
+                "Botão Clicado",
+                Toast.LENGTH_SHORT
+            ).show() })
     }
 }
